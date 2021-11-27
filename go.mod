239d7429909b71efed18ce2fe78ff8c2ebdc1c81
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-11-27 20:47:30 +0100

    Support to sync full namespace content

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/docker/cli v20.10.11+incompatible
 	github.com/google/go-containerregistry v0.7.0
+	github.com/philips-software/go-hsdp-api v0.51.1
 	github.com/spf13/viper v1.9.0
 )
 
@@ -14,8 +15,17 @@ require (
 	github.com/docker/docker v20.10.10+incompatible // indirect
 	github.com/docker/docker-credential-helpers v0.6.4 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/hasura/go-graphql-client v0.5.1 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mitchellh/mapstructure v1.4.2 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
@@ -29,9 +39,15 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
 	github.com/vbatts/tar-split v0.11.2 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20211111160137-58aab5ef257a // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20211110154304-99a53858aa08 // indirect
 	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/ini.v1 v1.63.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	nhooyr.io/websocket v1.8.6 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"strings"
@@ -10,6 +11,9 @@ import (
 	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/crane"
 	"github.com/google/go-containerregistry/pkg/name"
+	hsconfig "github.com/philips-software/go-hsdp-api/config"
+	"github.com/philips-software/go-hsdp-api/console"
+	"github.com/philips-software/go-hsdp-api/console/docker"
 	"github.com/spf13/viper"
 )
 
@@ -18,14 +22,19 @@ func main() {
 	viper.SetDefault("tags", "latest")
 	viper.AutomaticEnv()
 
+	ctx := context.Background()
 	sourceLogin := viper.GetString("source_login")
 	sourcePassword := viper.GetString("source_password")
 	sourceRepo := viper.GetString("source_repo")
 	sourceHost := viper.GetString("source_host")
 	sourceNamespace := viper.GetString("source_namespace")
 	if sourceLogin == "" || sourcePassword == "" || sourceHost == "" || sourceNamespace == "" {
-		fmt.Printf("some SOURCE details are missing\n")
+		fmt.Printf("some required SOURCE details are missing\n")
 		return
+	}
+	sourceRegion, err := hostRegion(sourceHost)
+	if err != nil {
+		fmt.Printf("SOURCE region error: %v\n", err)
 	}
 
 	tags := strings.Split(viper.GetString("tags"), ",")
@@ -36,15 +45,36 @@ func main() {
 
 	destLogin := viper.GetString("dest_login")
 	destPassword := viper.GetString("dest_password")
-	destRepo := viper.GetString("dest_repo")
 	destHost := viper.GetString("dest_host")
 	destNamespace := viper.GetString("dest_namespace")
-	if destLogin == "" || destPassword == "" || destHost == "" || destNamespace == "" {
-		fmt.Printf("some DEST details are missing\n")
+	if destNamespace == "" {
+		destNamespace = sourceNamespace
+	}
+	if destLogin == "" || destPassword == "" || destHost == "" {
+		fmt.Printf("some requires DEST details are missing\n")
 	}
 
 	if err := login(sourceHost, sourceLogin, sourcePassword); err != nil {
 		fmt.Printf("failed to login to SOURCE: %v\n", err)
+		return
+	}
+	sc, err := console.NewClient(nil, &console.Config{
+		Region: *sourceRegion,
+	})
+	if err != nil {
+		fmt.Printf("failed to provision console client: %v\n", err)
+		return
+	}
+	err = sc.Login(sourceLogin, sourcePassword)
+	if err != nil {
+		fmt.Printf("failed to login to SOURCE console: %v\n", err)
+		return
+	}
+	sourceDC, err := docker.NewClient(sc, &docker.Config{
+		Region: *sourceRegion,
+	})
+	if err != nil {
+		fmt.Printf("failed to provision docker client: %v\n", err)
 		return
 	}
 	if err := login(destHost, destLogin, destPassword); err != nil {
@@ -52,15 +82,58 @@ func main() {
 		return
 	}
 
-	for _, tag := range tags {
-		src := fmt.Sprintf("%s/%s/%s:%s", sourceHost, sourceNamespace, sourceRepo, tag)
-		dest := fmt.Sprintf("%s/%s/%s:%s", destHost, destNamespace, destRepo, tag)
-		err := crane.Copy(src, dest)
+	sourceNS, err := sourceDC.Namespaces.GetNamespaceByID(ctx, sourceNamespace)
+	if err != nil {
+		fmt.Printf("SOURCE namepace error: %v\n", err)
+		return
+	}
+
+	var reposToSync []string
+	if sourceRepo != "" {
+		reposToSync = append(reposToSync, fmt.Sprintf("%s/%s", sourceNamespace, sourceRepo))
+	} else { // All repos in namespace
+		repos, err := sourceDC.Namespaces.GetRepositories(ctx, sourceNS.ID)
 		if err != nil {
-			fmt.Printf("'%s' --> '%s' error: %v\n", src, dest, err)
+			fmt.Printf("error retrieving SOURCE repositories: %v\n", err)
 			return
 		}
+		for _, r := range *repos {
+			reposToSync = append(reposToSync, r.ID)
+		}
 	}
+
+	for _, repoToSync := range reposToSync {
+		tag, err := sourceDC.Repositories.GetLatestTag(ctx, repoToSync)
+		if err != nil {
+			fmt.Printf("skipping: no latest tag for '%s'\n", repoToSync)
+			continue
+		}
+		tagName := ":latest"
+		if tag.Name != "" {
+			tagName = ":" + tag.Name
+		}
+		src := fmt.Sprintf("%s/%s%s", sourceHost, repoToSync, tagName)
+		dest := fmt.Sprintf("%s/%s%s", destHost, repoToSync, tagName)
+		fmt.Printf("copying: %s -> %s\n", src, dest)
+		err = crane.Copy(src, dest)
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+			continue
+		}
+	}
+}
+
+func hostRegion(host string) (*string, error) {
+	sd, err := hsconfig.New()
+	if err != nil {
+		return nil, err
+	}
+	for _, region := range sd.Regions() {
+		if strings.Contains(sd.Region(region).Service("docker-registry").Host, host) {
+			return &region, nil
+		}
+	}
+	return nil, fmt.Errorf("region for '%s' not found", host)
 }
 
 func login(serverAddress string, user string, password string) interface{} {
